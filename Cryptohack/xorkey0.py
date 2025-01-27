bb7dbd855ae7f735d73fe4470b2d1e66a8d849ac
UrimThummim0 <rasolofondraiben02@gmail.com>
2025-01-27 18:40:39 +0300

    Monday

---
+++ Cryptohack/xorkey0.py
@@ -0,0 +1,18 @@
+"""
+For the next few challenges, you'll use what you've just learned to solve some more XOR puzzles.
+
+I've hidden some data using XOR with a single byte, but that byte is a secret. Don't forget to decode from hex first.
+
+73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d
+"""
+
+import base64
+
+ciphertext = "73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d"
+
+bytes_string = bytes.fromhex(ciphertext)
+print(bytes_string)
+
+for i in range(256):
+    plaintext = bytes(a ^ i for a in bytes_string)
+    print(i, plaintext)
